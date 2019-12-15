dd3c85de321d0083fc5d40e9353a5ef58cb0db2e
麦兜儿 <807067838@qq.com>
2019-12-15 16:51:36 +0800

    yy
    每部分内容分块

---
+++ 大事件项目/web_back/js/tools/user.js
@@ -0,0 +1,32 @@
+var user = {
+    login: function (options) {
+        $.ajax({
+            type: 'POST',
+            url: LOGIN,
+            data: {
+                user_name: options.data.user_name,
+                password: options.data.password
+            },
+            success: function (res) {
+                if (res.code == 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        })
+    },
+    logout: function (options) {
+        $.ajax({
+            type: 'POST',
+            url: LOGOUT,
+            success: function (res) {
+                if (res.code == 200) {
+                    options.success();
+                } else {
+                    options.fail();
+                }
+            }
+        })
+    }
+}
